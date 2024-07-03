6e91af50f3e4d27452abb58e0bdcb6a0eee0293c
Ramazan Oybolatov <ramazanoybolatov@gmail.com>
2024-07-03 22:53:30 +0300

    [PAP-1003] Добавил отображение кнопок при успешной авторизации

--- src/main/java/com/example/papricacookhousebot/handlers/NotAuthorizedHandler.java
+++ src/main/java/com/example/papricacookhousebot/handlers/NotAuthorizedHandler.java
@@ -5,17 +5,16 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static com.example.papricacookhousebot.config.BotMessagesConfiguration.*;
 import static com.example.papricacookhousebot.enums.Status.AUTHORIZED;
+import static com.example.papricacookhousebot.util.KeyboardHelper.*;
 
 @Component
 public class NotAuthorizedHandler extends StatusHandler {
-
-//    private ReplyKeyboard keyboard = n
 
     @Override
     public void process(Update update) {
         if (telegramBot.adminPasswordIsCorrect(update)) {
             if (transitionHelper.makeTransition(update, AUTHORIZED)) {
-                telegramBot.sendMessage(update, AUTHORIZATION_SUCCESSFUL);
+                telegramBot.sendMessage(update, AUTHORIZATION_SUCCESSFUL, OPEN_MENU_KEYBOARD);
             } else {
                 telegramBot.sendMessage(update, SOMETHING_WENT_WRONG);
             }
